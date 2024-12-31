89b206f4a7cc821c309298dfbcfcd7c954ad0764
marcinholsp <m183680@dac.unicamp.br>
2024-12-31 16:29:47 -0300

    Controllers de categorias e artigos criado

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const bodyParser = require('body-parser');
 const connection = require('./database/database.js');
 
+const categoriesController = require('./categories/CategoriesController');
+const articlesController = require('./articles/ArticlesController');
+
 //View engine setup
 app.set('view engine', 'ejs');
 
@@ -21,6 +24,10 @@ connection
     .catch((error) => {
         console.log('Error: ', error);
     });
+
+//Routes
+app.use('/', categoriesController);
+app.use('/', articlesController);
 
 app.get('/', (req, res) => {
   res.render('index.ejs');
