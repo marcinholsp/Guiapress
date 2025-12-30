80b9b0934827d28922198c0faea6e876d8378eb6
Márcio Levi <marciolevisalesprado@gmail.com>
2025-12-29 23:13:13 -0300

    Adiciona página para criação de novos artigos com formulário

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const Article = require('./Article');
 const router = express.Router();
 
 router.get('/articles', (req, res) => {
     res.send('Articles page');
+});
+
+router.get('/admin/articles/new', (req, res) => {
+    res.render('admin/articles/new');
+});
+
+router.post('/articles/save', (req, res) => {
+    let title = req.body.title;
+    let body = req.body.body;
+    if (title != undefined && body != undefined) {
+        Article.create({
+            title: title,
+            body: body
+        }).then(() => {
+            res.redirect('/admin/articles');
+        });
+    }
 });
 
 module.exports = router;
